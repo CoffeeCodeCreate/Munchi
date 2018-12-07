302d1d30eae6ae3f1e437f02549f4e4d82ce4212
Bryan Ramos <b.r.tech95@gmail.com>
2018-12-07 13:25:29 -0500

    Adding project to github

---
+++ app.js
@@ -0,0 +1,37 @@
+var express     = require('express'),
+app             = express(),
+bodyParser      = require("body-parser"),
+mongoose        = require("mongoose"),
+//MongoDB / Mongoose modules
+Restaurant = require("./models/restuarant");
+
+mongoose.connect("mongodb://localhost/munchi_v1");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req,res){
+    res.send("<h1>Im working!</h1>");
+});
+
+app.get("/restaurants", function(req,res){
+    //Get all restaurants from MongoDB database
+    Restaurant.find({}, function(err, allRestaurants){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("index", {restaurants: allRestaurants});
+        }
+    })
+});
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("app running....");
+});
---
+++ models/restuarant.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+
+var restaurantSchema = new mongoose.Schema({
+    name: String,
+    spectialty: String,
+    description: String,
+    bestSellers: 
+        [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Best"
+            }
+        ],
+    comments:
+    [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+//Export the restaurant schema as a mongoose model called Restaurant.
+module.exports = mongoose.model("Restaurant", restaurantSchema);
