1d70a32817d6e72786b88a3815858a01b28f4f62
Kilgore Trout <b.r.tech95@gmail.com>
2018-12-27 10:47:39 -0500

    middleware functionality added to the comments route

--- routes/comments.js
+++ routes/comments.js
@@ -8,9 +8,15 @@ var router = express.Router({mergeParams: true});
 var Restaurant = require("../models/restaurant");
 var Comment = require("../models/comment");
 
-// Comment Routes
+//middleware functions
+//note: It automatically retrieves the index.js file in this folder.
+var middleware = require("../middleware");
 
-router.get("/new", function(req,res){
+// Comment Routes
+/**
+ * Middleware function checks to see if user is logged in before they can comment.
+ */
+router.get("/new", middleware.isLoggedIn, function(req,res){
     //Find restaurant by ID
     Restaurant.findById(req.params.id, function(err, restaurant){
         if(err)
@@ -26,7 +32,7 @@ router.get("/new", function(req,res){
     });
 });
 
-router.post("/", function(req,res){
+router.post("/", middleware.isLoggedIn, function(req,res){
     //Look up restaurant based on ID
     Restaurant.findById(req.params.id, function(err, restaurant){
         if(err)
@@ -51,6 +57,59 @@ router.post("/", function(req,res){
                     res.redirect("/restaurants/" + restaurant._id);
                 }
             });
+        }
+    });
+});
+
+//comment edit
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req,res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err)
+        {
+            res.redirect("back");
+        }
+
+        else
+        {
+            /**
+             * Send the restaurant ID and the comment object to the edit form from req.body
+             */
+
+            res.render("comments/edit", {restaurant_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+//comment UPDATE route via PUT request
+router.put(":/comment_id",middleware.checkCommentOwnership, function(req,res){
+    /**
+     * Find the comment via the comment's id and then edit it.
+     */
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err)
+        {
+            res.redirect("back");
+        }
+        else
+        {
+            //redirect back to the restaurant based on ID
+            res.redirect("/restuarants/" + req.params.id);
+        }
+    });
+});
+
+//Comment DESTROY route via DELETE request
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req,res){
+    //find by ID and remove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err)
+        {
+            res.redirect("back");
+        }
+        else
+        {
+            //redirect back the show page of the restaurant based on ID
+            res.redirect("/restaurants/" , req.params.id);
         }
     });
 });
